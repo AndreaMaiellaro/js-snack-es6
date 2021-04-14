6a37743bda60f9f25514ce1144d5e732ce2037ec
shrekt444 <andrea.maie444@gmail.com>
2021-04-14 17:11:37 +0200

    secondo esercizio finito ma manca ancora un pezzo del primo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //es 1
-var bici = [
+const bici = [
     {
         'nome': 'alfredo',
         'peso': 10
@@ -18,7 +18,7 @@ var bici = [
     },
 ];
 
-var [ biciOne, biciTwo, biciThree, biciFour ] = bici;
+const [ biciOne, biciTwo, biciThree, biciFour ] = bici;
 console.log(
     'prima',biciOne.peso, 
     'seconda',biciTwo.peso, 
@@ -26,13 +26,11 @@ console.log(
     'quarta',biciFour.peso
 );
 
-
-
-var biciLeggera = biciOne;
+let biciLeggera = biciOne;
 console.log(biciLeggera);
 
-for (var i = 0; i < bici.length; i++ ) {
-    var thisBici = bici[i]
+for (let i = 0; i < bici.length; i++ ) {
+    const thisBici = bici[i];
 
     if (thisBici.peso < biciLeggera.peso) {
         biciLeggera = thisBici;
@@ -41,38 +39,55 @@ for (var i = 0; i < bici.length; i++ ) {
 
 console.log(biciLeggera);
 
+const { nome, peso } = biciLeggera;
+
+const testoLiteral = `
+    <ul>
+        <li>${nome}</li>
+        <li>${peso}</li>
+    </ul>
+`
+
 //es 2
-// var squadre = [
-//     {
-//         'nome': 'Torino',
-//         'punti fatti': 0,
-//         'falli subiti': 0
-//     },
-//     {
-//         'nome': 'Inter',
-//         'punti fatti': 0,
-//         'falli subiti': 0
-//     },
-//     {
-//         'nome': 'Juve',
-//         'punti fatti': 0,
-//         'falli subiti': 0
-//     },
-//     {
-//         'nome': 'Milan',
-//         'punti fatti': 0,
-//         'falli subiti': 0
-//     },
-// ];
+const squadre = [
+    {
+        'nome': 'Torino',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        'nome': 'Inter',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        'nome': 'Juve',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        'nome': 'Milan',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+];
 
+const [ squadreOne, squadreTwo, squadreThree, squadreFour ] = squadre;
+console.log(
+    squadreOne.nome,
+    squadreTwo.nome,
+    squadreThree.nome,
+    squadreFour.nome
+);
 
-// for ( var i = 0; i < squadre.length; i++) {
-//     var nuoviPunti = Math.floor((Math.random() * 50) + 1);
-//     var nuoviFalli = Math.floor((Math.random() * 30) + 1);
+for ( let i = 0; i < squadre.length; i++) {
 
-//     console.log(nuoviPunti);
-//     console.log(nuoviFalli);
+    const thisSquadra = squadre[i];
+    var nuoviPunti = Math.floor((Math.random() * 50) + 1);
+    var nuoviFalli = Math.floor((Math.random() * 30) + 1);
 
-//     var thisSquadra = squadre[i];
+    console.log(nuoviPunti);
+    console.log(nuoviFalli);
 
-// }
+    console.log(thisSquadra.nome, 'Falli subiti', nuoviFalli)
+}
